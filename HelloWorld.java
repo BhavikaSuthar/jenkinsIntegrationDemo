b4dcdc2db5ae2eb8865b214b1fc4a1947ea237d6
BhavikaSuthar <BhavikaSuthar2699@gmail.com>
2021-10-13 16:59:18 +0530

    Added new statements

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-	 System.out.println("Hello!"); 
+        	System.out.println("Hello, World!"); 
+	System.out.println("Hello There!"); 
+	System.out.println("How are you?"); 
+	System.out.println("I am Fine"); 
     }
 }
